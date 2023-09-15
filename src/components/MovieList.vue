023be0088f9166bdfac2c780c7be784aeda6c9b9
mauroluciano <mauroluciano911@gmail.com>
2023-09-15 08:45:58 +0200

    v-for don't work

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -5,23 +5,35 @@ export default {
   data() {
     return {
       title: "Lista dei film",
+      characters: [],
     };
   },
 
+  methods: {
+    fetchCharacters() {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
+        )
+        .then((response) => {
+          this.characters = response.data.results;
+        });
+    },
+  },
+
   created() {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
-      )
-      .then((res) => {
-        console.log(res);
-      });
+    this.fetchCharacters();
   },
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+  <div class="row">
+    <div class="col" v-for="character in characters" :key="character.id">
+      {{ character.name }}
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss"></style>
