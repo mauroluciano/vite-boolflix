effa97fc8dd87688dcbf2669f6add4fa87c01ed6
mauroluciano <mauroluciano911@gmail.com>
2023-09-15 11:04:41 +0200

    boh

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
-<script setup>
+<script>
 import AppHeader from "./components/AppHeader.vue";
 import MovieList from "./components/MovieList.vue";
 import AppMain from "./components/AppMain.vue";
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Lista dei film",
+      movies: [],
+    };
+  },
+
+  components: { AppHeader, MovieList, AppMain },
+
+  methods: {
+    fetchMovies() {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
+        )
+        .then((response) => {
+          this.movies = response.data.results;
+          console.log(response.data.results);
+        });
+    },
+  },
+
+  created() {
+    this.fetchMovies();
+  },
+};
 </script>
 
 <template>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,37 +1,14 @@
-<script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      title: "Lista dei film",
-      movies: [],
-    };
-  },
-
-  methods: {
-    fetchMovies() {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
-        )
-        .then((response) => {
-          this.movies = response.data.results;
-        });
-    },
-  },
-
-  created() {
-    this.fetchMovies();
-  },
-};
+<script setup>
+import { ref } from "vue";
 </script>
 
 <template>
   <h1>{{ title }}</h1>
   <div class="row">
-    <div class="col" v-for="movie in movies">
-      {{ movie.name }}
+    <div class="col">
+      <ul>
+        <li :v-for="movie in movies" :key="movie.id">{{ movie.title }}</li>
+      </ul>
     </div>
   </div>
 </template>
