c5aaa06f885ad69abd885cddb98dbd3f8e35e10c
mauroluciano <mauroluciano911@gmail.com>
2023-09-15 09:08:15 +0200

    a

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -5,24 +5,24 @@ export default {
   data() {
     return {
       title: "Lista dei film",
-      characters: [],
+      movies: [],
     };
   },
 
   methods: {
-    fetchCharacters() {
+    fetchMovies() {
       axios
         .get(
           "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
         )
         .then((response) => {
-          this.characters = response.data.results;
+          this.movies = response.data.results;
         });
     },
   },
 
   created() {
-    this.fetchCharacters();
+    this.fetchMovies();
   },
 };
 </script>
@@ -30,8 +30,8 @@ export default {
 <template>
   <h1>{{ title }}</h1>
   <div class="row">
-    <div class="col" v-for="character in characters" :key="character.id">
-      {{ character.name }}
+    <div class="col" v-for="movie in movies">
+      {{ movie.name }}
     </div>
   </div>
 </template>
