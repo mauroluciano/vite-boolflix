ec0d5053bc11ddd5dd1b7155904dd360d3ba9ff0
mauroluciano <mauroluciano911@gmail.com>
2023-09-15 08:32:42 +0200

    add api

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,27 @@
-<script setup>
+<script>
 import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Lista dei film",
+    };
+  },
+
+  created() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?query=batman&api_key=6de6c3fec90304b52c1b139cc3f8055e"
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  },
+};
 </script>
 
-<template>movie</template>
+<template>
+  <h1>{{ title }}</h1>
+</template>
 
 <style scoped></style>
